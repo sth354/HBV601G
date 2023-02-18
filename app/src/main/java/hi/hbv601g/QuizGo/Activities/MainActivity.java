1e2edf3f7046663bea822c02f7c59428841fe53b
sth354 <sth354@hi.is>
2023-02-18 12:10:14 +0000

    added correctAnswer, dummy stuff, user passwordhash

--- app/src/main/java/hi/hbv601g/QuizGo/Activities/MainActivity.java
+++ app/src/main/java/hi/hbv601g/QuizGo/Activities/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 
 import java.util.List;
 
-import hi.hbv601g.QuizGo.Entities.Question;
 import hi.hbv601g.QuizGo.Networking.NetworkCallback;
 import hi.hbv601g.QuizGo.Networking.NetworkManager;
 import hi.hbv601g.quizgo.R;
