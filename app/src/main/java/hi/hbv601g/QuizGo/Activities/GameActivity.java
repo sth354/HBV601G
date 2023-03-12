df568eed7b062afb9e3c502fba3eb92ad6b88b2c
frg13 <92691773+frg13@users.noreply.github.com>
2023-03-12 15:28:32 +0100

    Online Quiz API up and running

--- app/src/main/java/hi/hbv601g/QuizGo/Activities/GameActivity.java
+++ app/src/main/java/hi/hbv601g/QuizGo/Activities/GameActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,7 +49,20 @@ public class GameActivity extends AppCompatActivity {
 
         mGameService = new GameService();
         updateUsers();
-        mQuestions = mGameService.getQuestions();
+
+        //NEW THREAD TO MAKE THE API CALL ONLINE AND GENERATE QUESTIONS
+        Thread questionApi = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mQuestions = mGameService.getQuestions();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        questionApi.start();
+
         updateQuestion();
 
         //sending ids for now
@@ -75,7 +90,11 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void updateQuestion() {
-        mQuestion.setText(mQuestions[mCurrentQuestion].getQuestion());
+        try {
+            mQuestion.setText(mQuestions[mCurrentQuestion].getQuestion());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         mSeeAnswer.setText(R.string.seeAnswer);
         mCurrentQuestion++;
     }
